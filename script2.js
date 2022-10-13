bf7d332d8080be2de373d7b19627938b882b253f
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2022-10-13 12:18:13 +0800

    added new element

--- script2.js
+++ script2.js
@@ -1,7 +1,9 @@
 count = 0; 
 
 const value = document.querySelector("#number");
-const btns = document.querySelectorAll(".btn");
+const value2 = document.querySelector("#number-2")
+const btns = document.querySelectorAll(".btn-1");
+const btns2 = document.querySelectorAll(".btn-2")
 
 btns.forEach((btn) => {
     btn.addEventListener('click', (e) => {
@@ -27,5 +29,34 @@ btns.forEach((btn) => {
         value.style.color = "#222"
     }
     value.textContent = count;
+    });
+});
+
+count2 = 0
+
+btns2.forEach((btn2) => {
+    btn2.addEventListener('click', (e) => {
+        const styles2 = e.currentTarget.classList;
+    if(styles2.contains("decrease-number-2")){
+        count2--;
+    }else if (styles2.contains("increase-number-2")){
+        count2++;
+    } else if (styles2.contains("decrease-by-twos-2")) {
+        count2-=2;
+    } else if (styles2.contains("increase-by-twos-2")) {
+        count2+=2;
+    } else {
+        count2 = 0;
+    }
+    if(count2 > 0) {
+        value2.style.color = "green";
+    }
+    if(count2 < 0) {
+        value2.style.color = 'red';
+    }
+    if(count2 === 0) {
+        value2.style.color = "#222"
+    }
+    value2.textContent = count2;
     });
 });
